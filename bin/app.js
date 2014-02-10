3ea9964eb62245480eb0ca636232d869c5712a74
Trygve Lie <github@trygve-lie.com>
2014-02-10 12:34:24 +0100

    https everything

--- bin/app.js
+++ bin/app.js
@@ -10,9 +10,22 @@ var http                = require('http'),
 
 
 
+// Enforce SSL if the http header "x-forwarded-proto" is pressent
+// This header is set by the nodejitsu proxy which handles the SSL
+
+function ensureSSL(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === "http"){
+       res.redirect("https://" + req.headers.host + req.url);
+    }
+    return next();
+};
+
+
+
 app.disable('x-powered-by');
 
 app.configure('all',function () {
+    app.use(ensureSSL);
     app.use(express.compress());
     app.use(express.static(config.get('docRoot')));
 });
