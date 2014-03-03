dc91f9099771129d5ac6b393abb6dbd9840bfcd8
Trygve Lie <github@trygve-lie.com>
2014-03-03 19:19:19 +0100

    Adding twitter keys

--- bin/config.js
+++ bin/config.js
@@ -102,6 +102,34 @@ env: {
         format  : Array,
         default : ['/css/styles.min.css'],
         env     : "NODE_CSS_MIN_FILE"
+    },
+
+    twitterConsumerKey: {
+        doc     : "twitter API - consumer key",
+        format  : "*",
+        default : undefined,
+        env     : "TWITTER_CONSUMER_KEY"
+    },
+
+    twitterConsumerSecret: {
+        doc     : "twitter API - consumer secret",
+        format  : "*",
+        default : undefined,
+        env     : "TWITTER_CONSUMER_SECRET"
+    },
+
+    twitterAccessToken: {
+        doc     : "twitter API - access token",
+        format  : "*",
+        default : undefined,
+        env     : "TWITTER_ACCESS_TOKEN"
+    },
+
+    twitterAccessTokenSecret: {
+        doc     : "twitter API - access token secret",
+        format  : "*",
+        default : undefined,
+        env     : "TWITTER_ACCESS_TOKEN_SECRET"
     }
 
 });
